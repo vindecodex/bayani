a5eb89d7d02b8dbad00b4efa4dab2d871447bf98
vincent villaluna <villalunavincent@gmail.com>
2021-10-04 13:59:32 +0800

    :rocket: new bayani pakito

---
+++ core/bayanis/pakito.ts
@@ -0,0 +1,18 @@
+import { Bayani } from './bayani';
+import { BayaniList } from '../obj/bayanis';
+import { Attribute } from '../obj/attribute';
+
+class Pakito implements Bayani {
+	attribute: Attribute;
+	constructor(attribute = { name: "Pakito", health: 150, attackSpeed: 60, power: 30 }) {
+		this.attribute = attribute;
+	}
+	findTarget(bayaniList: BayaniList): Bayani {
+		return bayaniList.bayani[0];
+	}
+	execute(): void {
+		console.log("execute");
+	}
+}
+
+export const pakito = new Pakito();
