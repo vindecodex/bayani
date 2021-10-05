e787c6b95ac5c0f31cf63086b192745b5158e142
vincent villaluna <villalunavincent@gmail.com>
2021-10-05 15:28:27 +0800

    change register as index

--- core/bayanis/register.ts
+++
@@ -1,8 +0,0 @@
-import { bayaniMenu } from '../obj/bayanis';
-import { lapulapu } from './lapulapu';
-import { pakito } from './pakito';
-import { emilio } from './emilio';
-
-bayaniMenu.register(lapulapu);
-bayaniMenu.register(pakito);
-bayaniMenu.register(emilio);
