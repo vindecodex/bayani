6be4577cfd8c5e9f68b2d0a3a9c8f20ddf49548c
trisha ann balderas <centrii52@gmail.com>
2021-10-08 16:44:00 +0800

    add new hero

---
+++ core/bayanis/pedro.ts
@@ -0,0 +1,29 @@
+import { Bayani } from './bayani';
+import { BayaniList } from '../obj/bayanis';
+import { Attribute } from './attribute';
+
+class Pedro implements Bayani {
+	attribute: Attribute;
+	picked: boolean;
+	constructor(
+		attribute: Attribute = { 
+			name: "Pedro",
+			health: 100, 
+			totalHealth: 100,
+			attackSpeed: 30, 
+			power: 15 
+		},
+		picked = false
+	) {
+		this.attribute = attribute;
+		this.picked = picked;
+	}
+	findTarget(bayaniList: BayaniList): Bayani {
+		return bayaniList.bayani[0];
+	}
+	execute(): void {
+		console.log("execute");
+	}
+}
+
+export const lapulapu = new Pedro();
