d7ed8934a1204031e71179fc0e718afe5bb9acb1
vincent villaluna <villalunavincent@gmail.com>
2021-10-08 13:54:35 +0800

    added player total health

--- core/player/bossing.ts
+++ core/player/bossing.ts
@@ -4,22 +4,28 @@ import { Player } from './player';
 
 class Bossing implements Player {
 	health: number;
+	totalHealth: number;
 	name: string;
 	bayanis: BayaniList;
 
 	constructor(
 		health = 0, 
+		totalHealth = 0,
 		name = 'Bossing',
 		bayanis: BayaniList = { bayani: []},
 	) {
 		this.health = health;
+		this.totalHealth = totalHealth;
 		this.name = name;
 		this.bayanis = bayanis;
 	}
 
 	pickBayani(bayani: Bayani): void {
-		if (this.bayanis.bayani.length > 5) return;
-		this.bayanis.bayani.push(bayani);
+		if (this.bayanis.bayani.length < 5) {
+			this.bayanis.bayani.push(bayani);
+			this.totalHealth += bayani.attribute.totalHealth;
+			return;
+		}
 	}
 
 	calculateHealth(): void {
