9571c25348f84d8dd45e8e7fc658e5383c506d46
vincent villaluna <villalunavincent@gmail.com>
2021-10-11 10:43:17 +0800

    updated sample usage

--- core/index.ts
+++ core/index.ts
@@ -3,19 +3,35 @@ import newBossing from './player/bossing';
 import newBot from './player/bot';
 
 console.log('Initializing game!!!...');
-const g = newGame;
+const g = newGame();
 
 console.log('Setting up players!!!...');
 g.setPlayers(newBossing, newBot);
 
 console.log('Total of: ' + g.getBayaniMenu().bayani.length + ' bayanis');
+console.log(g.getBayaniMenu());
+
+console.log('===========PLAYER PICKS==============')
 
 g.playerAPick(0);
 g.playerAPick(1);
 g.playerAPick(2);
 g.playerAPick(3);
 g.playerAPick(4);
+g.playerAPick(10);
 
+console.log(g.getPlayerA().bayanis);
+
+
+console.log('===========UNPICKS==============')
 console.log(g.getBayaniUnpicked());
-
 console.log(g.getPlayerA().totalHealth);
+
+
+console.log('===========BOT PICKS==============')
+
+while(!g.getPlayerB().isReady()) {
+	g.randomPick(g.getPlayerB());
+}
+console.log(g.getPlayerB().bayanis);
+console.log(g.getPlayerB().totalHealth);
